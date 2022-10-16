67d1fa6b834cffe78a01385debe89099458e95d8
Documenter.jl <documenter@juliadocs.github.io>
2022-10-16 18:32:38 +0000

    build based on fde27cf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ConstantQ","category":"page"},{"location":"#ConstantQ","page":"Home","title":"ConstantQ","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ConstantQ.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ConstantQ]","category":"page"}]
+}
